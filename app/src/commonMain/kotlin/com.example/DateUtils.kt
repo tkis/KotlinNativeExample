d63056b9d6a1e155af6437f145ce2b441dd30c1c
Tomasz Kiszka <tomek.kiszka@gmail.com>
2020-12-09 02:02:09 +0100

    Minor update to comment

--- app/src/commonMain/kotlin/com.example/DateUtils.kt
+++ app/src/commonMain/kotlin/com.example/DateUtils.kt
@@ -1,7 +1,8 @@
 package com.example
 
-// Date is probably too simple example, however here's where you'd declare interface for accessing
-// stuff like Bluetooth, accelerometer etc.
+// Date is probably too simple example, however this is how you'd declare interface for accessing
+// stuff like Bluetooth, accelerometer etc, or wrappers for some things that might be still not supported
+// by the multiplatform library so you need to have a custom iOS implementation
 expect class DateUtils() {
     fun currentDateToString() : String
 }
